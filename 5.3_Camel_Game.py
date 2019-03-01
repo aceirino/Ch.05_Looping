e7e723616fb040ae286a778fb4f254a36f02e945
aceirino <aidankugley@gmail.com>
2019-03-01 12:17:06 -0600

    yaaaaaaaaæaahdf;asjdflasjdflkszjdfkljas2

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -4,6 +4,7 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 
 '''
+import random
 print("You are on the run from the...")
 print("You can choose to drink, moderate speed, speed up or stop for the night.")
 miles=0
@@ -28,3 +29,23 @@ while done==False:
         print("The natives are", natives, ("behind you"))
     elif user_choice.lower()=="d":
         tiredness==0
+    elif user_choice.lower()=="c":
+        miles+=random.randrange(10, 21)
+        print("miles traveled: ", miles)
+        natives=+random.randrange(7, 15)
+    elif user_choice.lower()=="b":
+        miles+=random.randrange(5, 13)
+        thirst+=1
+        tiredness=+1
+        natives+=random.randrange(7, 15)
+    elif user_choice.lower()=="a":
+        if drinksleft==0:
+            print("error")
+        else:
+            thirst==0
+            drinksleft+=-1
+    if thirst>6:
+        print("You died of thirst!")
+        break
+    elif thirst>4:
+        print("You are thirsty.")
