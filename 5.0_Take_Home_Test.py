7d81b1406a7c22e861dbbfe3046e057b7c3a4e4d
aceirino <aidankugley@gmail.com>
2019-03-25 12:03:29 -0500

    signed my name. My test is done!

--- 5.0_Take_Home_Test.py
+++ 5.0_Take_Home_Test.py
@@ -1,6 +1,6 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+but I will definitely not ask another person except the instructor. Signed: aidan kugley
  1. Make the following program work.
    '''
 print("This program takes three numbers and returns the sum.")
